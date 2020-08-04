a3f62a280bf0792aacf716dedb1efd539be70dd4
panyu <panyu@cvte.com>
2020-08-04 10:14:35 +0800

    second commit , commit files

---
+++ src/render/src/pages/Welcome.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+import { Card, Alert, Typography, Button } from 'antd';
+import styles from './Welcome.less';
+
+const { ipcRenderer } = require('electron')
+
+const CodePreview: React.FC<{}> = ({ children }) => (
+  <pre className={styles.pre}>
+    <code>
+      <Typography.Text copyable>{children}</Typography.Text>
+    </code>
+  </pre>
+);
+
+export default (): React.ReactNode => (
+  <PageContainer>
+    <Card>
+      <Alert
+        message="更快更强的重型组件，已经发布。"
+        type="success"
+        showIcon
+        banner
+        style={{
+          margin: -12,
+          marginBottom: 24,
+        }}
+      />
+      <Typography.Text strong>
+        高级表格{' '}
+        <a href="https://protable.ant.design/" rel="noopener noreferrer" target="__blank">
+          欢迎使用
+        </a>
+      </Typography.Text>
+      <CodePreview>yarn add @ant-design/pro-table</CodePreview>
+      <Typography.Text
+        strong
+        style={{
+          marginBottom: 12,
+        }}
+      >
+        高级布局{' '}
+        <a href="https://prolayout.ant.design/" rel="noopener noreferrer" target="__blank">
+          欢迎使用
+        </a>
+      </Typography.Text>
+      <CodePreview>yarn add @ant-design/pro-layout</CodePreview>
+      
+      <Button onClick={()=>{
+        console.log('ipcRenderer ===> ', ipcRenderer)
+        ipcRenderer.send('async-render', '我是来自渲染进程的同步消息');
+      }}>
+        测试主进程和渲染进程通信
+      </Button>
+
+
+    </Card>
+  </PageContainer>
+);
